21540245219a8ef7f3ae2682c593ef21900ccff4
Jacek Brożek <jac33k@gmail.com>
2014-05-29 15:36:56 +0200

    Revert "next time remember about skip-activerecord flag..."
    This reverts commit 600eb49d917354788c84b7e9e53f7e5dfc0041f9.

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'sprockets/railtie'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
