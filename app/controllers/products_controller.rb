c7c172eb4369640d6a45ee39c858e879c78a37fe
Jacek Brożek <jac33k@gmail.com>
2014-06-05 21:50:42 +0200

    instance variables are meant to be private - products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,42 +1,35 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   expose(:category)
+  expose(:products)
+  expose(:product)
 
-  # GET /products
   def index
-    @products = Product.all
   end
 
-  # GET /products/1
   def show
   end
 
-  # GET /products/new
   def new
-    @product = Product.new
   end
 
-  # GET /products/1/edit
   def edit
   end
 
-  # POST /products
   def create
-    @product = Product.new(product_params)
+    product = Product.new(product_params)
 
-    if @product.save
-      category.products << @product
-      redirect_to category_product_url(category, @product), notice: 'Product was successfully created.'
+    if product.save
+      category.products << product
+      redirect_to category_product_url(category, product), notice: 'Product was successfully created.'
     else
       render action: 'new'
     end
   end
 
-  # PATCH/PUT /products/1
   def update
-    if @product.update(product_params)
-      redirect_to category_product_url(category, @product), notice: 'Product was successfully updated.'
+    if product.update(product_params)
+      redirect_to category_product_url(category, product), notice: 'Product was successfully updated.'
     else
       render action: 'edit'
     end
@@ -44,16 +37,11 @@ class ProductsController < ApplicationController
 
   # DELETE /products/1
   def destroy
-    @product.destroy
-    redirect_to category_url(@product.category), notice: 'Product was successfully destroyed.'
+    product.destroy
+    redirect_to category_url(product.category), notice: 'Product was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
-
     def product_params
       params.require(:product).permit(:title, :description, :price, :category_id)
     end
