0ad8230fae215749ee7371c54617987fa80cac7b
Jacek Brożek <jac33k@gmail.com>
2014-06-05 22:33:00 +0200

    use strong parameters strategy for decent exposure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
+  decent_configuration do
+    strategy DecentExposure::StrongParametersStrategy
+  end
+
   protect_from_forgery with: :exception
 end
