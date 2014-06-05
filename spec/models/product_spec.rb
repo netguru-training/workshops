4b5f4d9335a2270da3d5d26f8d9221a52975c802
Jacek Brożek <jac33k@gmail.com>
2014-06-06 00:55:00 +0200

    validate price - decimal places

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,5 +5,13 @@ describe Product do
     it { should validate_presence_of :title }
     it { should validate_presence_of :description }
     it { should validate_presence_of :price }
+
+    describe '#price' do
+      let(:product) { build(:product, price: 1.234) }
+
+      it 'is limited to two decimal places' do
+        expect(product).to_not be_valid
+      end
+    end
   end
 end
