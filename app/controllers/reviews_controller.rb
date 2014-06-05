59d97fdf3b9c7e8da18365865bfc1e1fc4a43b93
Jacek Brożek <jac33k@gmail.com>
2014-06-06 01:04:55 +0200

    some basic stuff for reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,29 @@
+class ProductsController < ApplicationController
+
+  expose(:review)
+  expose(:product)
+
+  def edit
+  end
+
+  def create
+    self.review = Review.new(review_params)
+
+    if review.save
+      product.reviews << review
+      redirect_to category_product_url(product.category, product), notice: 'Review was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    review.destroy
+    redirect_to category_product_url(product.category, product), notice: 'Review was successfully destroyed.'
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:content, :rating)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :categories do
-    resources :products
+    resources :products do
+      resources :reviews
+    end
   end
 
   root 'categories#index'
