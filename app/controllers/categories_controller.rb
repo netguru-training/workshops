ac6507d6104898a6a3afa7a6c17ca0fdae384ed7
Jacek Brożek <jac33k@gmail.com>
2014-06-05 21:52:03 +0200

    instance variables are meant to be private - categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,59 +1,47 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy, :create]
+  
+  expose(:categories)
   expose(:category)
+  expose(:product) { Product.new }
 
-  # GET /categories
   def index
-    @categories = Category.all
   end
 
-  # GET /categories/1
   def show
-    @product = Product.new
   end
 
-  # GET /categories/new
   def new
-    @category = Category.new
   end
 
-  # GET /categories/1/edit
   def edit
   end
 
-  # POST /categories
   def create
-    @category = Category.new(category_params)
+    category = Category.new(category_params)
 
-    if @category.save
-      redirect_to @category, notice: 'Category was successfully created.'
+    if category.save
+      redirect_to category, notice: 'Category was successfully created.'
     else
       render action: 'new'
     end
   end
 
-  # PATCH/PUT /categories/1
   def update
-    if @category.update(category_params)
-      redirect_to @category, notice: 'Category was successfully updated.'
+    binding.pry
+    if category.update(category_params)
+      redirect_to category, notice: 'Category was successfully updated.'
     else
       render action: 'edit'
     end
   end
 
-  # DELETE /categories/1
   def destroy
-    @category.destroy
+    category.destroy
     redirect_to categories_url, notice: 'Category was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_category
-      @category = Category.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
     def category_params
       params.require(:category).permit(:name)
     end
