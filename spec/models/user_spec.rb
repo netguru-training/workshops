93b376bf2a5f067f68284d5af2946a10f353b29a
Adam Nowak <adam.nowak@netguru.pl>
2014-06-20 12:17:29 +0200

    check if user is admin

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,8 @@ require 'spec_helper'
 describe User do
   it { should validate_presence_of :firstname }
   it { should validate_presence_of :lastname }
+
+  it "by default isn't admin" do
+    expect(User.new).to_not be_admin
+  end
 end
