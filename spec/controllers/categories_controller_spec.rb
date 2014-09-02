9115aa5351a887dcf784c08f1fa825d337bab72c
Oskar Lakner <oskar.lakner@netguru.pl>
2014-09-02 16:22:32 +0200

    Removed commented out code, fixed double quotes.

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe CategoriesController do
 
-  let(:valid_attributes) { { name: "MyString" } }
+  let(:valid_attributes) { { name: 'MyString'} }
 
   let(:valid_session) { {} }
 
@@ -20,32 +20,32 @@ describe CategoriesController do
       controller.current_user.stub(admin?: false)
     end
 
-    describe "GET new" do
-      it "redirects user to the login page" do
+    describe 'GET new' do
+      it 'redirects user to the login page' do
         get :new, {}, valid_session
         expect(response).to redirect_to(new_user_session_path)
       end
     end
 
-    describe "GET edit" do
-      it "redirects user to the login page" do
+    describe 'GET edit' do
+      it 'redirects user to the login page' do
         category = Category.create! valid_attributes
         get :edit, { id: category.to_param }, valid_session
         expect(response).to redirect_to(new_user_session_path)
       end
     end
 
-    describe "POST create" do
+    describe 'POST create' do
       it 'redirects user to the login page' do
         post :create, {category: valid_attributes}, valid_session
         expect(response).to redirect_to(new_user_session_path)
       end
     end
 
-    describe "PUT update" do
-      it "redirect user to the login page" do
+    describe 'PUT update' do
+      it 'redirect user to the login page' do
         category = Category.create! valid_attributes
-        put :update, {:id => category.to_param, :category => { "name" => "MyString" }}, valid_session
+        put :update, {:id => category.to_param, :category => { 'name' => 'MyString'}}, valid_session
         expect(response).to redirect_to(new_user_session_path)
       end
     end
@@ -56,116 +56,116 @@ describe CategoriesController do
       controller.current_user.stub(admin?: true)
     end
 
-    describe "GET index" do
-      it "exposes all categories" do
+    describe 'GET index' do
+      it 'exposes all categories' do
         category = Category.create! valid_attributes
         get :index, {}, valid_session
         expect(controller.categories).to eq([category])
       end
     end
 
-    describe "GET show" do
-      it "exposes the requested category" do
+    describe 'GET show' do
+      it 'exposes the requested category' do
         category = Category.create! valid_attributes
         get :show, { id: category.to_param }, valid_session
         expect(controller.category).to eq(category)
       end
     end
 
-    describe "GET new" do
-      it "exposes a new category" do
+    describe 'GET new' do
+      it 'exposes a new category' do
         get :new, {}, valid_session
         expect(controller.category).to be_a_new(Category)
       end
     end
 
-    describe "GET edit" do
-      it "exposes the requested category" do
+    describe 'GET edit' do
+      it 'exposes the requested category' do
         category = Category.create! valid_attributes
         get :edit, { id: category.to_param }, valid_session
         expect(controller.category).to eq(category)
       end
     end
 
-    describe "POST create" do
-      describe "with valid params" do
-        it "creates a new Category" do
+    describe 'POST create' do
+      describe 'with valid params' do
+        it 'creates a new Category' do
           expect {
             post :create, {category: valid_attributes}, valid_session
           }.to change(Category, :count).by(1)
         end
 
-        it "exposes a newly created category as #category" do
+        it 'exposes a newly created category as #category' do
           post :create, {category: valid_attributes}, valid_session
           expect(controller.category).to be_a(Category)
           expect(controller.category).to be_persisted
         end
 
-        it "redirects to the created category" do
+        it 'redirects to the created category' do
           post :create, {category: valid_attributes}, valid_session
           expect(response).to redirect_to(Category.last)
         end
       end
 
-      describe "with invalid params" do
-        it "exposes a newly created but unsaved category" do
+      describe 'with invalid params' do
+        it 'exposes a newly created but unsaved category' do
           Category.any_instance.stub(:save).and_return(false)
-          post :create, {category: { "name" => "invalid value" }}, valid_session
+          post :create, {category: { 'name' => 'invalid value'}}, valid_session
           expect(controller.category).to be_a_new(Category)
         end
 
         it "re-renders the 'new' template" do
           Category.any_instance.stub(:save).and_return(false)
-          post :create, {category: { "name" => "invalid value" }}, valid_session
-          expect(response).to render_template("new")
+          post :create, {category: { 'name' => 'invalid value'}}, valid_session
+          expect(response).to render_template('new')
         end
       end
     end
 
-    describe "PUT update" do
+    describe 'PUT update' do
       let(:category) { Category.create! valid_attributes }
-      describe "with valid params" do
-        it "updates the requested category" do
-          Category.any_instance.should_receive(:update).with({ "name" => "MyString" })
-          put :update, {:id => category.to_param, :category => { "name" => "MyString" }}, valid_session
+      describe 'with valid params' do
+        it 'updates the requested category' do
+          Category.any_instance.should_receive(:update).with({ 'name' => 'MyString'})
+          put :update, {:id => category.to_param, :category => { 'name' => 'MyString'}}, valid_session
         end
 
-        it "exposes the requested category" do
+        it 'exposes the requested category' do
           put :update, {:id => category.to_param, :category => valid_attributes}, valid_session
           expect(controller.category).to eq(category)
         end
 
-        it "redirects to the category" do
+        it 'redirects to the category' do
           put :update, {:id => category.to_param, :category => valid_attributes}, valid_session
           response.should redirect_to(category)
         end
       end
 
-      describe "with invalid params" do
-        it "exposes the category" do
+      describe 'with invalid params' do
+        it 'exposes the category' do
           Category.any_instance.stub(:save).and_return(false)
-          put :update, {:id => category.to_param, :category => { "name" => "invalid value" }}, valid_session
+          put :update, {:id => category.to_param, :category => { 'name' => 'invalid value'}}, valid_session
           expect(controller.category).to eq(category)
         end
 
         it "re-renders the 'edit' template" do
           Category.any_instance.stub(:save).and_return(false)
-          put :update, {:id => category.to_param, :category => { "name" => "invalid value" }}, valid_session
-          response.should render_template("edit")
+          put :update, {:id => category.to_param, :category => { 'name' => 'invalid value'}}, valid_session
+          response.should render_template('edit')
         end
       end
     end
 
-    describe "DELETE destroy" do
+    describe 'DELETE destroy' do
       let!(:category) { Category.create! valid_attributes }
 
-      it "destroys the requested category" do
+      it 'destroys the requested category' do
         expect {
           delete :destroy, {:id => category.to_param}, valid_session
         }.to change(Category, :count).by(-1)
       end
 
-      it "redirects to the categories list" do
+      it 'redirects to the categories list' do
         delete :destroy, {:id => category.to_param}, valid_session
         response.should redirect_to(categories_url)
       end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -39,8 +39,6 @@ describe ProductsController do
     let(:product) { Product.create! valid_attributes }
 
     before do
-      # binding.pry
-      # @request.env["devise.mapping"] = Devise.mappings[:user]
       sign_in user2
       controller.stub(:user_signed_in?).and_return(true)
       controller.stub(:current_user).and_return(user2)
@@ -48,154 +46,154 @@ describe ProductsController do
       product.user = user
     end
 
-    describe "GET edit" do
-      describe "with valid params" do
-        it "redirects to product page" do
+    describe 'GET edit' do
+      describe 'with valid params' do
+        it 'redirects to product page' do
           get :edit, { id: product.to_param, category_id: category.to_param }, valid_session
           expect(response).to redirect_to(category_product_url(category, product))
         end
 
-        it "renders error message" do
+        it 'renders error message' do
           get :edit, { id: product.to_param, category_id: category.to_param }, valid_session
-          expect(controller.flash[:error]).to eq "You are not allowed to edit this product."
+          expect(controller.flash[:error]).to eq 'You are not allowed to edit this product.'
         end
       end
     end
 
-    describe "PUT update" do
-      describe "with valid params" do
-        it "redirects to product page" do
-          put :update, { id: product.to_param, product: { "title" => "MyString" }, category_id: category.to_param }, valid_session
+    describe 'PUT update' do
+      describe 'with valid params' do
+        it 'redirects to product page' do
+          put :update, { id: product.to_param, product: { 'title' => 'MyString' }, category_id: category.to_param }, valid_session
           expect(response).to redirect_to(category_product_url(category, product))
         end
 
-        it "does not update product" do
-          put :update, { id: product.to_param, product: { "title" => "MyNewString" }, category_id: category.to_param }, valid_session
-          expect(controller.product.title).to_not eq "MyNewString"
+        it 'does not update product' do
+          put :update, { id: product.to_param, product: { 'title' => 'MyNewString' }, category_id: category.to_param }, valid_session
+          expect(controller.product.title).to_not eq 'MyNewString'
         end
 
-        it "renders error message" do
-          put :update, { id: product.to_param, product: { "title" => "MyString" }, category_id: category.to_param }, valid_session
-          expect(controller.flash[:error]).to eq "You are not allowed to edit this product."
+        it 'renders error message' do
+          put :update, { id: product.to_param, product: { 'title' => 'MyString' }, category_id: category.to_param }, valid_session
+          expect(controller.flash[:error]).to eq 'You are not allowed to edit this product.'
         end
       end
     end
   end
 
-  describe "GET index" do
-    it "expose all products" do
+  describe 'GET index' do
+    it 'expose all products' do
       product = Product.create! valid_attributes
       get :index, {}, valid_session
       expect(controller.products).to eq([product])
     end
   end
 
-  describe "GET show" do
-    it "expose the requested product" do
+  describe 'GET show' do
+    it 'expose the requested product' do
       product = Product.create! valid_attributes
       get :show, { id: product.to_param, category_id: category.to_param }, valid_session
       expect(controller.product).to eq(product)
     end
   end
 
-  describe "GET new" do
-    it "expose a new product" do
+  describe 'GET new' do
+    it 'expose a new product' do
       get :new, { category_id: category.to_param }, valid_session
       expect(controller.product).to be_a_new(Product)
     end
   end
 
-  describe "GET edit" do
-    it "expose the requested product" do
+  describe 'GET edit' do
+    it 'expose the requested product' do
       product = Product.create! valid_attributes
       get :edit, { id: product.to_param, category_id: category.to_param }, valid_session
       expect(controller.product).to eq(product)
     end
   end
 
-  describe "POST create" do
-    describe "with valid params" do
-      it "creates a new Product" do
+  describe 'POST create' do
+    describe 'with valid params' do
+      it 'creates a new Product' do
         expect {
           post :create, { product: valid_attributes, category_id: category.to_param }, valid_session
         }.to change(Product, :count).by(1)
       end
 
-      it "expose a newly created product" do
+      it 'expose a newly created product' do
         post :create, { product: valid_attributes, category_id: category.to_param }, valid_session
         expect(controller.product).to be_a(Product)
         expect(controller.product).to be_persisted
       end
 
-      it "redirects to the created product" do
+      it 'redirects to the created product' do
         post :create, { product: valid_attributes, category_id: category.to_param }, valid_session
         response.should redirect_to(category_product_url(category, Product.last))
       end
     end
 
-    describe "with invalid params" do
-      it "expose a newly created but unsaved product" do
+    describe 'with invalid params' do
+      it 'expose a newly created but unsaved product' do
 
         Product.any_instance.stub(:save).and_return(false)
-        post :create, { product: { "title" => "invalid value" }, category_id: category.to_param }, valid_session
+        post :create, { product: { 'title' => 'invalid value' }, category_id: category.to_param }, valid_session
         expect(controller.product).to be_a_new(Product)
       end
 
       it "re-renders the 'new' template" do
         Product.any_instance.stub(:save).and_return(false)
-        post :create, { product: { "title" => "invalid value" }, category_id: category.to_param }, valid_session
-        response.should render_template("new")
+        post :create, { product: { 'title' => 'invalid value' }, category_id: category.to_param }, valid_session
+        response.should render_template('new')
       end
     end
   end
 
-  describe "PUT update" do
-    describe "with valid params" do
-      it "updates the requested product" do
+  describe 'PUT update' do
+    describe 'with valid params' do
+      it 'updates the requested product' do
         product = Product.create! valid_attributes
-        Product.any_instance.should_receive(:update).with({ "title" => "MyString" })
-        put :update, { id: product.to_param, product: { "title" => "MyString" }, category_id: category.to_param }, valid_session
+        Product.any_instance.should_receive(:update).with({ 'title' => 'MyString' })
+        put :update, { id: product.to_param, product: { 'title' => 'MyString' }, category_id: category.to_param }, valid_session
       end
 
-      it "expose the requested product" do
+      it 'expose the requested product' do
         product = Product.create! valid_attributes
         put :update, { id: product.to_param, product: valid_attributes, category_id: category.to_param }, valid_session
         expect(controller.product).to eq(product)
       end
 
-      it "redirects to the product" do
+      it 'redirects to the product' do
         product = Product.create! valid_attributes
         put :update, { id: product.to_param, product: valid_attributes, category_id: category.to_param }, valid_session
         response.should redirect_to(category_product_url(category, product))
       end
     end
 
-    describe "with invalid params" do
-      it "expose the product" do
+    describe 'with invalid params' do
+      it 'expose the product' do
         product = Product.create! valid_attributes
         Product.any_instance.stub(:save).and_return(false)
-        put :update, { id: product.to_param, product: { "title" => "invalid value" }, category_id: category.to_param }, valid_session
+        put :update, { id: product.to_param, product: { 'title' => 'invalid value' }, category_id: category.to_param }, valid_session
         expect(controller.product).to eq(product)
       end
 
       it "re-renders the 'edit' template" do
         product = Product.create! valid_attributes
         Product.any_instance.stub(:save).and_return(false)
-        put :update, { id: product.to_param, product: { "title" => "invalid value" }, category_id: category.to_param }, valid_session
-        response.should render_template("edit")
+        put :update, { id: product.to_param, product: { 'title' => 'invalid value' }, category_id: category.to_param }, valid_session
+        response.should render_template('edit')
       end
     end
   end
 
-  describe "DELETE destroy" do
-    it "destroys the requested product" do
+  describe 'DELETE destroy' do
+    it 'destroys the requested product' do
       product = Product.create! valid_attributes
       expect {
         delete :destroy, { id: product.to_param, category_id: category.to_param }, valid_session
       }.to change(Product, :count).by(-1)
     end
 
-    it "redirects to the category page" do
+    it 'redirects to the category page' do
       product = Product.create! valid_attributes
       delete :destroy, { id: product.to_param, category_id: category.to_param }, valid_session
       response.should redirect_to(category_url(category))
