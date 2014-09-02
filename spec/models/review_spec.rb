4897c0c68770cf4b930dccaa87861c626697e32d
Oskar Lakner <oskar.lakner@netguru.pl>
2014-09-02 16:18:52 +0200

    Updated review spec to validate user_id

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,6 +4,7 @@ describe Review do
   describe 'validations' do
     it { should validate_presence_of :content }
     it { should validate_presence_of :rating }
+    it { should validate_presence_of :user_id }
     it { should belong_to :user }
 
   end
