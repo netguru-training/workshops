1fe5c1b906f465d1a911b231be6f6e9477731eca
blazejhadzik <hadzikblazej@gmail.com>
2014-09-10 11:52:44 +0200

    spec helper update.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,7 +24,8 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
-  
+  config.raise_errors_for_deprecations!
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
   end
